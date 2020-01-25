f462872f572cfb919742ddf175cb32b393ec1476
kowalix007 <kowalix007@gmail.com>
2020-01-25 16:58:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,5 +4,11 @@ require 'pry'
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  
+  attr_accessor :email 
+  def initialize(email)
+    @email = email
+  end 
+  def parse
+    
+  end
 end 
